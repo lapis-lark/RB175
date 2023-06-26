978d1abfcc16fd2873717d05f080bb366a64bf6d
lapis <lapis.santuri@gmail.com>
2023-06-25 18:04:44 -0600

    book server work

--- 3/book_viewer/book_viewer.rb
+++ 3/book_viewer/book_viewer.rb
@@ -7,3 +7,11 @@ get "/" do
   @toc = File.readlines("data/toc.txt")
   erb :home
 end
+
+get "/chapters/1" do
+  @title = "Sherlock Holmes"
+  @chapter = "Chapter 1"
+  @toc = File.readlines("data/toc.txt")
+  @text = File.read("data/chp1.txt")
+  erb :chapters
+end
--- 3/test.rb
+++
@@ -1,7 +0,0 @@
-require 'rack'
-class MyApp
-  def call(env)
-    ['200', { "Content-Type" => "text/plain" }, [env.to_s]]
-  end
-end
-Rack::Handler::WEBrick.run MyApp.new
