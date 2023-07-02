f826da1d04981316f0dd3a9636bae438a61781e7
lapis <lapis.santuri@gmail.com>
2023-07-02 16:56:22 -0600

    lesson 5 complete

---
+++ file_based_cms/test/test_cms.rb
@@ -0,0 +1,31 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "history.txt"
+    assert_includes last_response.body, "changes.txt"
+  end
+
+  def test_viewing_text_document
+    get '/about.txt'
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "soaring amidst the clouds"
+  end
+end
