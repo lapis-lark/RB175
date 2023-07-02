7fbcb4e973830403a03623febb5c420d132a4a6e
lapis <lapis.santuri@gmail.com>
2023-07-02 16:12:21 -0600

    lesson 3 complete

--- file_based_cms/cms.rb
+++ file_based_cms/cms.rb
@@ -3,6 +3,15 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require "tilt/erubis"
 
-get '/' do
-  'Getting started'
+root = File.expand_path("..", __FILE__)
+
+before do
+  @title = "CMS"
+end
+
+get "/" do
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index
 end
