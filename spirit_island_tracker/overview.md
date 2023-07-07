f2fd3c0c257095cff16114a1e2a2c44cd55f3dd8
lapis <lapis.santuri@gmail.com>
2023-07-07 15:07:23 -0600

    index page complete

--- spirit_island_tracker/overview.md
+++ spirit_island_tracker/overview.md
@@ -15,3 +15,18 @@
   - view statistics for all games played
   - edit game info (only user)
   - index of all users linking to their statistics page
+
+
+
+REQUIREMENTS:
+  Make an index page listing all users
+    Link to the user page
+
+
+STEPS:
+  get '/'
+    Supply all user information to index.erb
+    iterate over users, print each, link to user page
+
+  get '/users/:name'
+    blank for now
--- spirit_island_tracker/spirit_island_tracker.rb
+++ spirit_island_tracker/spirit_island_tracker.rb
@@ -1,30 +1,21 @@
-=begin
-  Data structure for spirit island results
-
-  {users
-    {games
-      win/lose: 
-      adversary:
-      level: (0 unless adversary selected)
-      ...
-    }
-
-  }
-
-
-
-
-
-
-
-=end
-
 require 'sinatra'
-require 'sinatra/contrib'
+require 'sinatra/reloader' if development?
 require "tilt/erubis"
 require 'date'
+require 'yaml'
+
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
 
 get '/' do
+  
+  @users = YAML.load(File.read(data_path + "/users.yml"), permitted_classes: [Time])
   erb :index
 end
 
