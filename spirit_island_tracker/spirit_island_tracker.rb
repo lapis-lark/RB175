b3955d587e421ab7f0d1853b096d0380e91a0fb8
lapis <lapis.santuri@gmail.com>
2023-07-08 11:17:26 -0600

    reworked data structure, improved player page

--- spirit_island_tracker/spirit_island_tracker.rb
+++ spirit_island_tracker/spirit_island_tracker.rb
@@ -13,31 +13,40 @@ def data_path
   end
 end
 
-def load_users_data
-  YAML.load(File.read(data_path + "/users.yml"), permitted_classes: [Time])
+def load_game_data
+  YAML.load(File.read(data_path + "/games.yml"), permitted_classes: [Time])
 end
 
 def generate_game_id
-  users_data = load_users_data
-  ids =[]
-  
-  users_data.each { |user| ids << users_data[user].keys }
-  return 0 if ids.empty?
-  ids.map.sort[-1] + 1
+  ids = @game_data.keys
+  ids.empty? ? 0 : ids[-1] + 1
+end
+
+def get_user_games(user)
+  user_games = @game_data.select { |_, data| data["players"].include?(user) }
 end
 
 def display_win_information(user)
-  user_data = load_users_data[user]
-  games = user_data.keys.size
-  wins = user_data.select { |k, _| user_data[k]["win?"] == "true" }.size
-  losses = games - wins
+  user_games = get_user_games(user)
+  total_games = user_games.keys.size
+  wins = user_games.select { |_, data| data["win?"] == "true" }.size
+  losses = total_games - wins
 
-  "#{games} game(s) played (#{wins} win(s), #{losses} losses)"
+  "#{total_games} game(s) played (#{wins} win(s), #{losses} losses)"
   # user_data.inspect
 end
 
+def join_and(arr)
+  arr[0...-1].join(', ') + ", and #{arr[-1]}"
+end
+
+def display_spirits_vs_adversary(data)
+  spirits = join_and(data["players"].values)
+  "#{spirits} vs. #{data['adversary']} Level #{data['level']}"
+end
+
 before do
-  @users_data = load_users_data
+  @game_data = load_game_data
 end
 
 get '/' do
@@ -47,11 +56,13 @@ end
 
 get '/users/:user' do |user|
   @user = user
+  @user_games = get_user_games(user)
   erb :test_user_page
 end
 
 get '/users/:user/games/:game_id' do |user, game_id|
   @game_id = game_id.to_i
+  @game = @game_data[@game_id]
   @user = user
   # return @users_data[@user][@game_id].inspect
   erb :game_data
